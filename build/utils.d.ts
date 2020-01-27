6f211dd3c76984825dbbd88f28adf6a3697798f0
dominic.griesel <d.griesel@gmx.net>
2020-01-27 09:55:11 +0100

    chore: update ioBroker typings

--- build/utils.d.ts
+++ build/utils.d.ts
@@ -3,11 +3,30 @@
 export declare const controllerDir: string;
 /** Reads the configuration file of JS-Controller */
 export declare function getConfig(): Record<string, any>;
+/**
+ * This type is used to include and exclude the states and objects cache from the adaptert type definition depending on the creation options
+ */
+export declare type AdapterInstance = Omit<ioBroker.Adapter, "oObjects" | "oStates">;
+declare type AdapterInstanceType<T extends ioBroker.AdapterOptions> = T extends {
+    objects: true;
+    states: true;
+} ? AdapterInstance & {
+    oObjects: Exclude<ioBroker.Adapter["oObjects"], undefined>;
+    oStates: Exclude<ioBroker.Adapter["oStates"], undefined>;
+} : T extends {
+    objects: true;
+} ? AdapterInstance & {
+    oObjects: Exclude<ioBroker.Adapter["oObjects"], undefined>;
+} : T extends {
+    states: true;
+} ? AdapterInstance & {
+    oStates: Exclude<ioBroker.Adapter["oStates"], undefined>;
+} : AdapterInstance;
 interface AdapterConstructor {
-    new (adapterName: string): ioBroker.Adapter;
-    new (adapterOptions: ioBroker.AdapterOptions): ioBroker.Adapter;
-    (adapterName: string): ioBroker.Adapter;
-    (adapterOptions: ioBroker.AdapterOptions): ioBroker.Adapter;
+    new (adapterName: string): AdapterInstance;
+    new <T extends ioBroker.AdapterOptions>(adapterOptions: T): AdapterInstanceType<T>;
+    (adapterName: string): AdapterInstance;
+    <T extends ioBroker.AdapterOptions>(adapterOptions: ioBroker.AdapterOptions): AdapterInstanceType<T>;
 }
 /** Creates a new adapter instance */
 export declare const adapter: AdapterConstructor;
